6849a0a9b34603e77654d771e17530d52bcd8d6f
ZuckXu <zaikunxu1989@gmail.com>
2017-02-28 11:47:09 +0100

    update

--- semantic_augment/util.py
+++ semantic_augment/util.py
@@ -4,16 +4,44 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 
-def plot_query_result(pkl_fn, outputfn):
-    data = pickle.load(open(pkl_fn, 'rb'))
-    concetps = [x[0] for x in data]
-    sims =     [x[1] for x in data]
-    plt.plot(sims)
-    if not os.path.exists('query_sim'):
-        os.mkdir('query_sim')
+def getDict():
+    title2ind, ind2title = {}, {}
+    if not os._exists('title_map.pkl'):
+        lines = open('title.txt', 'r').readlines()
+        lines = [x.strip() for x in lines]
+        ind2title = dict(enumerate(lines))
+        title2ind = dict([(line, i) for i, line in enumerate(lines)])
 
-    #plt.savefig('query_sim/' + outputfn)
-    ##plt.close()
+        with open('title_map.pkl', 'w') as fp :
+            pickle.dump(title2ind, fp)
+            pickle.dump(ind2title,  fp)
+    else:
+        with open('title_map.pkl', 'r') as fp :
+            pickle.load(title2ind, fp)
+            pickle.load(ind2title, fp)
+    return title2ind, ind2title
+
+def get_query_result(pkl_fn):
+    data = pickle.load(open(pkl_fn, 'rb'))
+    concepts = [x[0] for x in data]
+    sims =     [x[1] for x in data]
+    #indices =  [x[2] for x in data]
+    return concepts,  sims
+
+def cal_tag_tag_sim(tag_pkls):
+    Tag_tag_topk_sims = []
+    for tag_pkl in tag_pkls:
+        print(tag_pkl)
+        concepts, sims = get_query_result('query_pkl/' + tag_pkl)
+        gradient = np.gradient(sorted(sims))
+        sorted_sim_indices = np.argsort(sims)
+        topk_gradient_indices = np.where(gradient > 0.005)[0]
+        topk_sim_indices = sorted_sim_indices[topk_gradient_indices][:-1]
+        topk_sim = np.array(sims)[topk_sim_indices]
+        topk_concepts = np.array(concepts)[topk_sim_indices]
+        Tag_tag_topk_sims.append( [topk_sim_indices, topk_concepts, topk_sim])
+        print(topk_sim_indices, topk_concepts, topk_sim)
+    return Tag_tag_topk_sims
 
 def check_wierd_query(pkl_fn):
     data = pickle.load(open(pkl_fn, 'rb'))
@@ -38,18 +66,21 @@ def extract_title():
     title2ind = dict([ (x, i+1) for i, x in enumerate(data)])
     pickle.dump(title2ind, open('title2ind.pkl', 'wb'))
 
+
 def calculate_idf_all():
-
-    f = open('wiki_tfidf/_bow.mm')
     result = {}
-    for piece in read_in_chunks(f):
-        tmp = piece.strip().split()
-        if re.search('[a-zA-Z]', tmp[1]):
-            continue
-        word = tmp[1]
-        if word in result.keys():
-            result[word] += 1
-        else:
-            result[word] = 1
+    with open("wiki_tfidf/_bow.mm") as infile:
+        for i, piece in enumerate(infile):
 
+            tmp = piece.strip().split()
+
+            if i < 2:
+                continue
+            word = tmp[1]
+            if i % 10000 == 0 :
+                print(i, word)
+            if word in result.keys():
+                result[word] += 1
+            else:
+                result[word] = 1
     pickle.dump(result, open('idf.pkl', 'wb'))
